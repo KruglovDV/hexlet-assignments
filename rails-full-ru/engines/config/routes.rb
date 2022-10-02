43d7bf38203c19c9c256509d9a4629160d58434b
@hexlet/cli <support@hexlet.io>
2022-10-02 13:54:45 +0300

    submit rails-full-ru/engines

---
+++ rails-full-ru/engines/config/routes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # BEGIN
+  mount RailsStats::Engine, at: '/stats'
+  # END
+
+  scope module: :web do
+    root 'home#index'
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ rails-full-ru/engines/engines/rails_stats/config/routes.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RailsStats::Engine.routes.draw do
+  # BEGIN
+  root 'stats#index'
+  # END
+end
